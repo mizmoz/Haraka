81b2b03ac112f7e6476c6f78b8600827c8e12f0a
Ian Chadwick <chad@mizmoz.com>
2016-05-02 14:34:47 +0100

    Remove the X-Priority header otherwise mail clients will set the items as important or not which is probably not what we want to be doing when the plugin is active

--- plugins/queue/priority.js
+++ plugins/queue/priority.js
@@ -1,5 +1,6 @@
 /**
- * Attempt to set the message priority by read the contents of the mail headers and looking for the X-Priority header
+ * Attempt to set the message priority by reading the contents of the mail headers and looking for the X-Priority header
+ * This plugin will remove the X-Priority header
  *
  * @param next
  * @param connection
@@ -9,11 +10,17 @@ exports.hook_data_post = function (next, connection) {
     var transaction = connection.transaction;
 
     // check if the X-Priority header is set and that some other plugin hasn't already set the priority
-    if (typeof transaction.header.headers['x-priority'] === 'object'
-        && typeof transaction.notes.priority === 'undefined') {
-        // set the priority on the notes so we can use this later in outbound.js
-        var priority = parseInt(transaction.header.headers['x-priority'][0]);
-        transaction.notes.priority = priority;
+    if (typeof transaction.header.headers['x-priority'] === 'object') {
+
+        // check the priority hasn't already been set in another plugin
+        if (typeof transaction.notes.priority === 'undefined') {
+            // use the priority from the header and save in the notes for use later in outbound.js
+            var priority = parseInt(transaction.header.headers['x-priority'][0]);
+            transaction.notes.priority = priority;
+        }
+
+        // remove the X-Priority header otherwise emails will be marked as important which probably isn't what we want
+        transaction.remove_header('x-priority');
     }
 
     return next(CONT);
